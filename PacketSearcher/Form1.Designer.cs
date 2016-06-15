5aefe8212c87074a938af922c54ee500e047885c
NewDelion <el.emena.crea@gmail.com>
2016-06-16 00:57:44 +0900

    Initial Commit mpe.asmからパケットのidとreadメソッドの内容を抽出するところまでできた プロジェクトのプロパティからコンソールアプリケーションにして動かしてください

---
+++ PacketSearcher/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PacketSearcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(434, 19);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9F);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 37);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(405, 347);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "Instruction";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(423, 37);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(316, 347);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "Analyzed Field";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(452, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "PacketList";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(533, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 396);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ PacketSearcher/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PacketSearcher
+{
+    public partial class Form1 : Form
+    {
+        PacketCollection pkColl = new PacketCollection();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (pkColl.exists(textBox1.Text))
+                {
+                    Packet pk = pkColl.get(textBox1.Text);
+                    string instr = "";
+                    foreach (string line in pk.instr)
+                        instr += line + "\n";
+                    richTextBox1.Text = instr.Substring(0, instr.Length - 1);
+                }
+                else
+                {
+                    MessageBox.Show("パケットが見つかりませんでした。");
+                }
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "逆アセのリザルトファイルを選択してください";
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                this.Text = "解析中...";
+                await Task.Run(() =>
+                {
+                    using (StreamReader reader = new StreamReader(openFileDialog1.FileName))
+                    {
+                        long linesCnt = 0;
+                        while (reader.Peek() != -1)
+                        {
+                            linesCnt++;
+                            string line = reader.ReadLine().TrimEnd();
+                            if (line.IndexOf("SharedConstants::NetworkProtocolVersion") != -1)
+                            {
+                                Console.WriteLine("Line {0}: Reading...", linesCnt);
+                                Console.WriteLine(" Detecting protocol version...");
+                                linesCnt++;
+                                string next = reader.ReadLine().Trim();
+                                Match line_protocol = Regex.Match(next, "[0-9a-f]+:[ \t]+([0-9a-f]+)");
+                                if (line_protocol.Success)
+                                {
+                                    Console.WriteLine("Detected {0}", Convert.ToUInt32(line_protocol.Groups[1].Value, 16));
+                                }
+                            }
+                            else if (Regex.IsMatch(line, @"([A-Za-z0-9_]+Packet)::getId\(\)"))
+                            {
+                                Match line_packet_getid = Regex.Match(line, @"([A-Za-z0-9_]+Packet)::getId\(\)");
+                                string name = line_packet_getid.Groups[1].Value;
+
+                                Console.WriteLine("Line {0}: Reading...", linesCnt);
+                                Console.WriteLine(" Detecting packet-to-ID declaration...");
+                                linesCnt++;
+                                string next = reader.ReadLine().Trim();
+                                Match line_id = Regex.Match(next, "[0-9a-f]{4}[ \t]+movs[ \t]+r0, #([0-9]+)");
+                                if (line_id.Success)
+                                {
+                                    uint id = Convert.ToUInt32(line_id.Groups[1].Value, 16) + 0x8e;
+                                    Console.WriteLine(" Detected {0}", id);
+                                    pkColl.get(name).id = id;
+                                }
+                            }
+                            else if (Regex.IsMatch(line, @"^[0-9a-f]+ \<([A-Za-z0-9_]+Packet)::read\(RakNet::BitStream\*\)\>:"))
+                            {
+                                Match line_instr_start = Regex.Match(line, @"^[0-9a-f]+ <([A-Za-z0-9_]+Packet)::read\(RakNet::BitStream\*\)>:");
+                                Console.WriteLine("Line {0}: Reading...", linesCnt);
+                                string pkName = line_instr_start.Groups[1].Value;
+                                Packet pk = pkColl.get(pkName);
+                                pk.startAnalyze();
+                                while (reader.Peek() != -1)
+                                {
+                                    linesCnt++;
+                                    Console.CursorLeft = 0;
+                                    Console.Write("Line {0}: Reading {1} packet structure...", linesCnt, pkName);
+                                    line = reader.ReadLine().TrimEnd();
+                                    if (line.Length - 2 != line.TrimStart(' ').Length)
+                                        break;
+                                    pk.analyze(line);
+                                }
+                                Console.WriteLine();
+                                pk.stopAnalyze();
+                                
+                            }
+                        }
+                    }
+                });
+                this.Text = "解析完了！！";
+                this.button1.Enabled = true;
+                this.button2.Enabled = true;
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            foreach (Packet pk in pkColl.packets)
+                result += string.Format("{0,3}: {1}\n", pk.id, pk.name);
+            richTextBox1.Text = result.Substring(0, result.Length - 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Description = "パケットデータを保存するディレクトリ選んでくだせえ";
+            if (folderBrowserDialog1.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (Packet pk in pkColl.packets)
+                {
+                    string filename = string.Format("{0}_0x{1}.pk", pk.name, pk.id.ToString("x2"));
+                    File.WriteAllLines(folderBrowserDialog1.SelectedPath + "\\" + filename, pk.instr.ToArray());
+                }
+            }
+            MessageBox.Show("保存しました！！");
+        }
+    }
+
+    public class Packet
+    {
+        public uint id;
+        public string name;
+        public List<string> instr = new List<string>();
+
+        public Packet(string name)
+        {
+            this.name = name;
+        }
+
+        public void startAnalyze()
+        {
+            this.instr = new List<string>();
+        }
+
+        public void analyze(string line)
+        {
+            this.instr.Add(line);
+        }
+
+        public void stopAnalyze()
+        {
+
+        }
+    }
+
+    public class PacketCollection
+    {
+        public List<Packet> packets = new List<Packet>();
+
+        public List<Packet> getPackets()
+        {
+            return this.packets;
+        }
+
+        public Packet get(string name)
+        {
+            if (!packets.Exists(d => d.name.Equals(name)))
+                this.packets.Add(new Packet(name));
+            return this.packets.Find(d => d.name.Equals(name));
+        }
+
+        public bool exists(string name)
+        {
+            return packets.Exists(d => d.name.Equals(name));
+        }
+    }
+}
